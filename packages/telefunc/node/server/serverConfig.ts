00d8dd3d54f9d528f8ffe72144040861528c2510
Rom <git@brillout.com>
2026-05-28 17:38:44 +0200

    fix: use Vite's `root` as default for `config.root` (#262)

--- packages/telefunc/node/server/serverConfig.ts
+++ packages/telefunc/node/server/serverConfig.ts
@@ -1,5 +1,6 @@
 export { configUser as config }
 export { getServerConfig }
+export { setRootFromVite }
 export type { ConfigUser }
 export type { ConfigResolved }
 
@@ -7,7 +8,7 @@ import { assertUsage } from '../../utils/assert.js'
 import { hasProp } from '../../utils/hasProp.js'
 import { isObject } from '../../utils/isObject.js'
 import { isTelefuncFilePath } from '../../utils/isTelefuncFilePath.js'
-import { toPosixPath, pathIsAbsolute } from '../../utils/path.js'
+import { toPosixPath, pathIsAbsolute, assertPosixPath } from '../../utils/path.js'
 
 /** Telefunc Server Configuration */
 type ConfigUser = {
@@ -96,6 +97,11 @@ function getServerConfig(): ConfigResolved {
       if (configUser.root) {
         return toPosixPath(configUser.root)
       }
+      // Doesn't work for Cloudflare Workers — but we can make it work if we want to, see reverted commits at https://github.com/telefunc/telefunc/pull/262
+      if (rootFromVite) {
+        assertPosixPath(rootFromVite)
+        return rootFromVite
+      }
       if (typeof process == 'undefined' || !hasProp(process, 'cwd')) return null
       return toPosixPath(process.cwd())
     })(),
@@ -167,4 +173,9 @@ function validateUserConfig(configUserUnwrapped: ConfigUser, prop: string, val: 
   }
 
   return true
+}
+
+let rootFromVite: string | null = null
+function setRootFromVite(root: string) {
+  rootFromVite = root
 }
